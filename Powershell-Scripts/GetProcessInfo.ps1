4fe8703c32b12d89448ef603511019b5b6519a5f
pranavhirulkar21 <127311810+pranavhirulkar21@users.noreply.github.com>
2024-05-31 13:15:48 +0530

    powershell scripts added

---
+++ Powershell-Scripts/GetProcessInfo.ps1
@@ -0,0 +1,45 @@
+﻿Write-Output "Process Management"
+
+$num = Read-Host "Select the option.
+1. Show all the processes.
+2. Search process by it's name.
+3. Search process by it's ID.
+4. Show 10 processes consuming amount most of CPU.
+5. Show 10 processes having most no. of handles.
+6. Show 10 processes consuming least amount of CPU.
+7. Show 10 processes having least no. of handles.`n"
+
+
+switch($num){
+1 {
+Get-Process
+}
+
+2 {
+$name = Read-Host "Enter the name of the process "
+Get-Process -Name $name
+}
+
+3 {
+$id = Read-Host "Enter the ID of the process "
+Get-Process -Id $id
+}
+
+4 {
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
+}
+
+5 {
+Get-Process | Sort-Object Handles -Descending | Select-Object -First 10
+}
+
+6 {
+Get-Process | Sort-Object CPU -Descending | Select-Object -Last 10
+}
+
+7 {
+Get-Process | Sort-Object Handles -Descending | Select-Object -Last 10
+}
+
+default { Write-Output "Invalid Option !"}
+}
---
+++ Powershell-Scripts/SetFilePermission.ps1
@@ -0,0 +1,84 @@
+﻿function Set-Permission{
+param(
+[string[]]$FilePath,
+[string[]]$UserName,
+[string[]]$permi,
+[int[]]$num
+)
+
+
+
+$num = Read-Host "`n
+1. To view the Owner of File/Folder.
+2. To view Permissions of the File.
+3. To Grant Specific Permissions to a User.
+4. To Remove Specific Permissions from a User
+5. To copy permission from one folder and give it to another.
+6. To Change ownership of a File/Folder.
+0. To Exit. `n"
+
+switch($num) {
+1 { 
+$FilePath = Read-Host "Enter the file path."
+Get-Acl -Path $FilePath
+}
+
+2 {
+$FilePath = Read-Host "Enter the file path."
+(Get-Acl -Path $FilePath).Access | Format-Table *
+}
+
+3 {
+$FilePath = Read-Host "Enter the file path."
+$UserName = Read-Host "Enter the UserName to Grant permission."
+$permi = Read-Host "Enter the Modified Permission."
+
+$acl = Get-Acl -Path $FilePath
+$accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule($UserName, $permi, "Allow")
+$acl.SetAccessRule($accessrule)
+$acl | Set-Acl -Path $FilePath
+(Get-ACL -Path $FilePath).Access | Format-Table *
+}
+
+4 {
+$FilePath = Read-Host "Enter the file path."
+$UserName = Read-Host "Enter the UserName to Remove permission."
+$permi = Read-Host "Enter the Modified Permission."
+
+$acl = Get-Acl -Path $FilePath
+$accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule($UserName, $permi, "Allow")
+$acl.RemoveAccessRule($accessrule)
+$acl | Set-Acl -Path $FilePath
+(Get-ACL -Path $FilePath).Access | Format-Table *
+}
+
+5 {
+$FilePath1 = Read-Host "Enter the file path from where you wanna copy permissions."
+$FilePath2 = Read-Host "Enter the file path to whom you wanna give permissions."
+
+​Get-ACL -Path $FilePath1 | Set-ACL -Path $FilePath2
+(Get-ACL -Path $FilePath2).Access | Format-Table *
+
+}
+
+6 {
+$FilePath = Read-Host "Enter the file path."
+$UserName = Read-Host "Enter the New User's Name."
+
+$Acl = Get-Acl -Path $FilePath
+$User = New-Object System.Security.Principal.Ntaccount($UserName)
+$Acl.SetOwner($User)
+$Acl | Set-Acl -Path $FilePath
+Get-ACL -Path $FilePath
+}
+
+0 { Break }
+
+default {Write-Output "Invalid Input!!!"}
+
+
+}
+
+}
+
+Set-Permission
